cdde890bc96bc44b7cd5382fadc7323301485228
young <young20050727@gmail.com>
2024-07-18 13:15:56 +0800

    feat: add script for testing

---
+++ script/sync_executable.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+TEST_MACHINE_1=192.168.0.100
+USER=young
+TARGET_PATH=/home/$USER/minesweeper
+
+scp build/minesweeper $USER@$TEST_MACHINE_1:$TARGET_PATH
---
+++ script/test_multi.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+bash script/build.sh
+
+bash script/sync_executable.sh
